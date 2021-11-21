8a1393f2862c39b099d2f054bd65b121ea3ab81a
Youssef Hajjari <youssefhajjari01@gmail.com>
2021-11-21 12:54:28 +0100

    setup redux with typescrip and types

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import StackNavigator from "./StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import store from "@/src/app/store";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import type { AppState } from '@/src/app/store'
+
+export interface UserState {
+  user: null | any,
+  status: 'idle' | 'loading' | 'failed'
+}
+
+const initialState: UserState = {
+  user: null,
+  status: 'idle',
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.user = null
+    },
+    setUser: (state, action: PayloadAction<number>) => {
+      state.user += action.payload
+    },
+  },
+})
+
+export const { logout, setUser } = userSlice.actions
+
+export const selectCount = (state: AppState) => state.user
+
+export default userSlice.reducer
