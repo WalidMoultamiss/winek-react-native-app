0fe31f626ab77e877252fb3963144107f1460ffa
YounessHassoune <yhassoune7@gmail.com>
2021-11-23 10:31:24 +0100

    adding safe area provider

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import StackNavigator from "./src/navigation/StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import store from "./src/store/store";
 import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
     </Provider>
   );
 }
